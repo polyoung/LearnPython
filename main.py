2832bbabb0fdf1289be4868a549e249926a6ad26
dy <dai_dy@yeah.net>
2023-02-09 17:01:19 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+flag = False
+"""
+这是多行注释
+时间
+这是时间为什么看不见
+"""
+if flag:
+    print("这是true")
+else:
+    print("这是false")
+
+name = input("按下enter键，其他任意键...\n")
+print(name)
+
+s = 'abcdef'
+b = s[1:5]
+print(b)
